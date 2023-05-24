6736f3f40604046e33bd927f21bf4694eac20acf
Balakumaran <balakumaran247@gmail.com>
2023-05-24 14:14:48 +0530

    updated: jaltol route to POST with Schema

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from src.gpt import gpt_query
+from src.utils import JaltolInput
 
 app = FastAPI(title="JaltolAI", version="0.0.1")
 templates = Jinja2Templates(directory="templates")
@@ -10,7 +11,7 @@ templates = Jinja2Templates(directory="templates")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request":request})
 
-@app.post("/query/")
-async def query(request: Request):
-    item_data = await request.json()
-    return gpt_query(item_data['input'])
+@app.post("/jaltol/")
+async def jaltol(input: JaltolInput):
+    input_dict = input.dict()
+    return gpt_query(input_dict['user'])
