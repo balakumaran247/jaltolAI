5883005559bec3e66657e4c603204bb9cc8d4965
Balakumaran <balakumaran247@gmail.com>
2023-06-01 17:47:49 +0530

    updated: TemplateResponse with static

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from src.gpt import gpt_query
 from src.utils import JaltolInput
 
-app = FastAPI(title="JaltolAI", version="0.0.1")
+__version__ = "0.0.1"
+
+app = FastAPI(title="JaltolAI", version=__version__)
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
+
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request":request})
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post("/jaltol/")
 async def jaltol(input: JaltolInput):
     input_dict = input.dict()
-    return gpt_query(input_dict['user'])
+    return gpt_query(input_dict["user"])
