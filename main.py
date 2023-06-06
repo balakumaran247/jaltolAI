38dd75c7995dd97571bd684332ef7ab360083199
Balakumaran <balakumaran247@gmail.com>
2023-06-06 17:28:26 +0530

    added: JSON Response

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from src.gpt import gpt_query
-from src.utils import JaltolInput
+from src.utils import JaltolInput, JaltolOutput
 
 __version__ = "0.0.1"
 
@@ -17,7 +17,8 @@ async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.post("/jaltol/")
-async def jaltol(input: JaltolInput):
+@app.post("/jaltol/", response_model=JaltolOutput)
+async def jaltol(input: JaltolInput) -> JaltolOutput:
     input_dict = input.dict()
-    return gpt_query(input_dict["user"])
+    payload = {'text': gpt_query(input_dict["user"])}
+    return JSONResponse(content=payload)
