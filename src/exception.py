b5d98f0886387aad54f6897b875026d7a5ba7c77
Balakumaran <balakumaran247@gmail.com>
2023-06-14 17:29:44 +0530

    added: exception formatter

---
+++ src/exception.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def log_e(error):
+    e_c, e_m, exc_tb = sys.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    return f"Error occured in [{file_name}], line [{exc_tb.tb_lineno}], [{e_c.__name__}: {e_m}]"
+
+
+if __name__ == "__main__":
+    try:
+        1 / 0
+    except Exception as e:
+        print(log_e(e))
