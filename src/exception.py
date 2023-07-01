3535b21765b81a045bf1426831073f887512b05e
Balakumaran <balakumaran247@gmail.com>
2023-07-01 13:01:48 +0530

    added: logging

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,13 @@
 import sys
 
 
-def log_e(error):
+def log_e() -> str:
+    """
+    Log the details of an exception.
+
+    Returns:
+        str: A formatted string containing information about the error.
+    """
     e_c, e_m, exc_tb = sys.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     return f"Error occured in [{file_name}], line [{exc_tb.tb_lineno}], [{e_c.__name__}: {e_m}]"
