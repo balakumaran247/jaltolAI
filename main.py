c2a245bd0925b4c501d9d94f2883fb6c7de91e4f
Balakumaran <balakumaran247@gmail.com>
2023-07-14 23:07:57 +0530

    added: ee credential check and create

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.sessions import SessionMiddleware
 
-from src.gpt import AgentHandler
-from src.utils import JaltolInput, JaltolOutput
-
 __version__ = "0.0.2"
 
 _ = load_dotenv(find_dotenv())
@@ -22,6 +19,22 @@ app = FastAPI(title="JaltolAI", version=__version__)
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_KEY"), max_age=7200)
+
+credential_dir_path = os.path.join(os.path.expanduser("~"), ".config", "earthengine")
+credential_file_path = os.path.join(credential_dir_path, "credentials")
+if os.path.isfile(credential_file_path):
+    logger.info("EE credential file is available!")
+else:
+    logger.info("EE credential file not found!")
+    ee_token = os.getenv("EE_TOKEN")
+    credential = '{"refresh_token":"%s"}' % ee_token
+    os.makedirs(credential_dir_path, exist_ok=True)
+    with open(credential_file_path, "w") as file:
+        file.write(credential)
+    logger.info("EE credential file created.")
+
+from src.gpt import AgentHandler
+from src.utils import JaltolInput, JaltolOutput
 
 
 @app.get("/", response_class=HTMLResponse)
